b2733b746b4f74372e54b75da0208746e7b80eb2
rukshaan <shaanruk0309@gmail.com>
2025-07-31 20:28:50 +0530

    login signup ui updated1.15

--- chatapp/src/App.jsx
+++ chatapp/src/App.jsx
@@ -1,13 +1,23 @@
+import { useState } from 'react';
 import Chat from "./components/chat/Chat"
 import Detail from "./components/detail/Detail"
 import List from "./components/list/List"
+import Login from "./components/login/Login"
 
 const App = () => {
+  const [user, setUser] = useState(null); // Simulated auth
+
   return (
     <div className='container'>
-      <List/>
-      <Chat/>
-      <Detail/>
+      {
+        user ? (
+          <>
+          <List/>
+          <Chat/>
+          <Detail/>
+          </>
+        ): (<Login/>)
+      }
     </div>
   )
 }
